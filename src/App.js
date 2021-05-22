e40000156b165691f5cafde9d3e5848665061c79
Beverly <beverlybuscher@gmail.com>
2021-05-22 15:04:30 -0500

    added functional component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+function HelloFromComponent() {
+  return <h2>Hello from functional component!</h2>
+}
+
 // what looks like html is actually jsx
 
 function App() {
+  const Hello = "Hello World from JS!";
   return (
     <div className="App">
       <header className="App-header">
+         <HelloFromComponent />
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
+        <h2>Hello World</h2>
+        <h3>{Hello}</h3>
+        <HelloFromComponent />
+        
         <a
           className="App-link"
           href="https://reactjs.org"
