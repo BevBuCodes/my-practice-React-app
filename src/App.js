1f7b18d4abffbc995b0cc8354c449d68a1d7e6a7
Beverly <beverlybuscher@gmail.com>
2021-05-22 15:23:32 -0500

    added components folder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
-function HelloFromComponent() {
-  return <h2>Hello from functional component!</h2>
-}
-
-// what looks like html is actually jsx
+import HelloFromComponent from './components/HelloFromFunctionalComponent'
 
 function App() {
-  const Hello = "Hello World from JS!";
+  const Hello = <h2>Hello World from JS</h2>;
   return (
     <div className="App">
       <header className="App-header">
          <HelloFromComponent />
 
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         
+
         <h2>Hello World</h2>
-        <h3>{Hello}</h3>
+        {Hello}
         <HelloFromComponent />
         
         <a
---
+++ src/components/HelloFromFunctionalComponent.js
@@ -0,0 +1,5 @@
+function HelloFromComponent() {
+  return <h2>Hello World from functional component!</h2>
+}
+
+export default HelloFromComponent;
