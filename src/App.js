97d8cfe025ca9dda1be2bf5586f80d6890dfc13c
Beverly <beverlybuscher@gmail.com>
2021-05-22 17:31:56 -0500

    added class component.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloFromComponent from './components/HelloFromFunctionalComponent'
+import HelloFromClassComponent from './components/HelloFromClassComponent'
 
 function App() {
   const Hello = <h2>Hello World from JS</h2>;
   return (
     <div className="App">
       <header className="App-header">
-         <HelloFromComponent />
+      
 
         <img src={logo} className="App-logo" alt="logo" />
         
 
-        <h2>Hello World</h2>
+        <h2 className="greeting">Hello World</h2>
         {Hello}
         <HelloFromComponent />
+        <HelloFromClassComponent/>
         
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+
       </header>
     </div>
   );
---
+++ src/components/HelloFromClassComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HelloFromClassComponent extends React.Component{
+
+render () {
+    return <h2>Hello from class component!</h2>
+}
+
+
+
+
+
+}
+
+export default HelloFromClassComponent;
